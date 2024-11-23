9657c6d0208f933ae2d95c51c26dcb57f7f7e3de
Ilyos <ilyosnarziyev6@gmail.com>
2024-11-23 18:19:20 +0500

    added cards section

--- js/index.js
+++ js/index.js
@@ -21,6 +21,9 @@ tailwind.config = {
         btn_color: "#009379",
         text_color: "#2D2D2D",
       },
+      boxShadow: {
+        'shadow': '34.85px 29.63px 48.34px 0px #3366FF0D',
+      },
     },
     letterSpacing: {
       tightest: "-0.025em"
